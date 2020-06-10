882eeeb0279bb99de7d5405bdb84e0caad602454
sioneptune <sioneptune@gmail.com>
2020-06-10 20:36:42 +0200

    Boids now avoid the walls

--- boids.py
+++ boids.py
@@ -1,4 +1,5 @@
 import random
+import sys
 import pygame
 import time
 from math import cos, sin, tan, atan, sqrt, radians, degrees
@@ -12,14 +13,14 @@ YELLOW = (255, 255, 0)
 GREY = (100, 100, 100)
 
 wsize = 800 # Size of the window
-BOID_SIZE = 10
+BOID_SIZE = 5
 
 class Boid:
     
-    PERSONAL_SPACE = 25
+    PERSONAL_SPACE = 15
     VINCINITY = 75
-    ANGLE_INCREMENT = 1
-    DISTANCE = 5
+    ANGLE_INCREMENT = 5
+    DISTANCE = 1.5
     
     def __init__(self,x,y):
         self.x = x
@@ -37,37 +38,65 @@ class Boid:
         self.y = pos[1]
     
     def move(self):
+        # print(self.angle)   
+        self.choose_next_move([])
         self.angle = self.next_move
+        self.keep_angle_bounds()
         self.x += Boid.DISTANCE * cos(radians(self.angle))
         self.y += Boid.DISTANCE * sin(radians(self.angle))
+        self.resolve_oob()
+        
     
-    def choose_next_move(self, neary_boids):
-        #if(self.close_wall() is not None):
+    def choose_next_move(self, neary_boids):           
+        if self.y < Boid.VINCINITY: # Near Top
+                self.choose_right() if self.to_my_left("TOP") else self.choose_left()
+        if wsize - self.y < Boid.VINCINITY:   # Near Bottom
+                self.choose_right() if self.to_my_left("BOTTOM") else self.choose_left()
+        if self.x < Boid.VINCINITY:   # Near Left
+                self.choose_right() if self.to_my_left("LEFT") else self.choose_left()
+        if wsize - self.x < Boid.VINCINITY:   # Near Right
+                self.choose_right() if self.to_my_left("RIGHT") else self.choose_left()
+        #print(self.position)
+        # print(self.angle)
         return
         
+    def resolve_oob(self):
+        if self.x < 0:
+            self.x = 0
+        if self.x > wsize:
+            self.y = wsize
+        if self.y < 0:
+            self.y = 0
+        if self.y > wsize:
+            self.y = wsize
+        
+    def to_my_left(self, item):
+        if item=="TOP":
+            return self.angle < 90 and self.angle > -90
+        if item=="BOTTOM":
+            return self.angle > 90 or self.angle < -90
+        if item=="LEFT":
+            return self.angle < 0
+        if item=="RIGHT":
+            return self.angle > 0
+    
     def choose_left(self):
         self.next_move = self.angle - Boid.ANGLE_INCREMENT
         
     def choose_right(self):
         self.next_move = self.angle + Boid.ANGLE_INCREMENT
+       
+    def keep_angle_bounds(self):
+        if self.angle > 180:
+            self.angle -= 360
+        if self.angle < -180:
+            self.angle += 360
         
     def distance_from(self,boid):
         return ((self.x - boid.x)**2 + (self.y - boid.y)**2)**0.5
     
     def boids_in_vincinity(self, boids):
-        return [b for b in boids if self.distance_from(b) <= Boid.VINCINITY and b is not self]
-    
-    def close_wall(self):
-        if self.x < PERSONAL_SPACE:
-            return "TOP"
-        elif wsize - self.x < PERSONAL_SPACE:
-            return "BOTTOM"
-        elif self.y < PERSONAL_SPACE:
-            return "LEFT"
-        elif wsize - self.y < PERSONAL_SPACE:
-            return "RIGHT"
-        else:
-            return None 
+        return [b for b in boids if self.distance_from(b) <= Boid.VINCINITY and b is not self]      
         
 
 def pg_init():
@@ -77,12 +106,12 @@ def pg_init():
     pygame.display.set_caption("Lol I kinda tolerate life")
     return screen
     
-def run():
+def run(nb_boids):
     keep_going = True
     clock = pygame.time.Clock()
     screen = pg_init()
     
-    boids = [Boid(400,400)]  # populate here
+    boids = [Boid(400,400) for x in range(nb_boids)]  # populate here
     
     while keep_going:
         # Event handling
@@ -102,4 +131,8 @@ def run():
         clock.tick(60)
     pygame.quit()
     
-run()
+if __name__=="__main__":
+    if(len(sys.argv) == 1):
+        print("Please specify the number of boids")
+    else:
+        run(int(sys.argv[1]))
